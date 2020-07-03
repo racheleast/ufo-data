731bd49541b809db20ce5b85ad960dfb3263080b
Matthew Johns <3mjohns@gmail.com>
2020-07-03 14:01:25 -0700

    add year of sighting

--- matt/scratch.R
+++ matt/scratch.R
@@ -14,7 +14,7 @@ data <- read_csv("nuforc_reports.csv")
 
 data$primary_key <- 1:nrow(data)
 
-
+data$sighting_year <- lubridate::year(data$date_time)
 
 
 shape <- data %>% group_by(shape) %>% summarise(count = n()) %>% arrange(desc(count))
@@ -32,4 +32,8 @@ states <- data %>% group_by(state) %>% summarise(count = n()) %>% arrange(desc(c
 
 ggplot(states, aes(x = reorder(state, -count), y = count)) + geom_col() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = .35))
+
+
+year <- data %>% group_by(sighting_year) %>% summarise(count = n()) #%>% arrange(desc(count))
+
 
