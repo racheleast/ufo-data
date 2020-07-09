0924e3a5405964e78257a8e09cc0e893696b7259
racheleast <rachel.east@yahoo.com>
2020-07-09 16:47:51 -0700

    Map with Nuclear Power Plants

--- rachel/rachScratch.R
+++ rachel/rachScratch.R
@@ -13,8 +13,21 @@
 
 #ggmap(bw_map) + geom_point(data = data, aes(x= longitudes, y=latitudes))
 
+#split_df2 <- split(newdata2$data.sighting_year, newdata2$data.sighting_year > 2005)
 
+#split_df2[1]
 
+#earlydata <- data.frame(split_df2[1], newdata2$data.city_longitude, newdata2$data.city_latitude )
+
+#earlydata <- with(newdata2, split(newdata2$data.sighting_year > 2005, newdata2$data.city_longitude, newdata2$data.city_latitude))
+
+#split_df <- split(year, year > 2005)
+
+#split_df
+
+#year$firstyears <- year<2005
+
+#firstyears
 
 # This piece of code just plots our long/lat with no map behind it
 
